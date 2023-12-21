976b8db16e2f0aac0175b3ccb048951c38da4103
techvishal1988 <104866564+techvishal1988@users.noreply.github.com>
2023-12-21 22:07:58 +0530

    Update server.js

--- gitops-application-sample-master/server.js
+++ gitops-application-sample-master/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to GitOps Platform\n 2oth Dec- k8s what is happeining');
+  res.send('Welcome to GitOps Platform\n 2oth Dec- k8s what is happeining-12345rt');
 });
 
 app.listen(PORT, HOST);
