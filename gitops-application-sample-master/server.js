8ef49082f4737a2d056cc68199af7351be2c5ac1
Vishal Gupta <techbie22@outlook.com>
2023-12-22 12:30:34 +0530

    changes_done

--- gitops-application-sample-master/server.js
+++ gitops-application-sample-master/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome 22nd Dec-2k23');
+  res.send('Welcome 22nd Dec-2k24');
 });
 
 app.listen(PORT, HOST);
