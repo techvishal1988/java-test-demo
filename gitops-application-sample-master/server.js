30bed57ad4d209958e2101bcaa8f5674c177e754
techvishal1988 <104866564+techvishal1988@users.noreply.github.com>
2023-12-22 10:24:04 +0530

    Update server.js

--- gitops-application-sample-master/server.js
+++ gitops-application-sample-master/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Kubernetes world');
+  res.send('Welcome to Kubernetes world-1233');
 });
 
 app.listen(PORT, HOST);
