c1c2fcca7037a2284642ee3f7fc56b4e35085549
techvishal1988 <104866564+techvishal1988@users.noreply.github.com>
2023-12-26 14:36:34 +0530

    Update server.js

--- gitops-application-sample-master/server.js
+++ gitops-application-sample-master/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome 22nd Dec-MArry Chrismas----New year coming');
+  res.send('Welcome 22nd Dec-MArry Chrismas----New year coming123');
 });
 
 app.listen(PORT, HOST);
